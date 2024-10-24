6f57cee5b182c93f445001f83064863b835dd827
Artem Gafarov <a.m.gafarov@ya.ru>
2024-10-24 18:10:25 +0300

    Yandex GPT 4

--- internal/services/textProcessing.go
+++ internal/services/textProcessing.go
@@ -239,7 +239,7 @@ func (s *Service) ProcessText(model, prompt, text, temperature string) (string, 
 }
 
 func (s *Service) getMaxQueueLength() uint8 {
-	return 5
+	return 1
 }
 
 func (s *Service) getCurrentQueueLength() uint8 {
