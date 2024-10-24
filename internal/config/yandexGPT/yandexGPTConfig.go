adf4625b40679ae6102f8f7b3382014ea7baaab6
Artem Gafarov <a.m.gafarov@ya.ru>
2024-10-24 18:43:37 +0300

    Yandex GPT 4

--- internal/config/yandexGPT/yandexGPTConfig.go
+++ internal/config/yandexGPT/yandexGPTConfig.go
@@ -4,7 +4,7 @@ const (
 	GPT_API_KEY = "AQVNzU5pE0yY9za2gCmN9dGxE0B0nb6hKm8aWzdX"
 	STORAGE_ID  = "b1gjtlqofdt5mu5io6a9"
 	URI         = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-	MAX_TOKENS  = "2000"
+	MAX_TOKENS  = "32000"
 
 	DEFAULT_TEMPERATURE = "0.3"
 	DEFAULT_MODEL       = "pro"
